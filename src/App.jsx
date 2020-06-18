c66fa6b54b4761a8b3050c994792025e29e41505
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-18 14:19:28 +0800

    added bootstrap icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="App-title">Music Master</div>
-        <div>
-          <input placeholder="Search artist.."/>
-          <button>button</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+            type="text"
+            placeholder="Search an artist..."
+            />
+            <Button variant="outline-secondary">
+              <BsSearch />
+            </Button>
+          </InputGroup>
+
+        </FormGroup>
         <div className="profile">
           <div>Artist picture</div>
           <div>Artist Name</div>
