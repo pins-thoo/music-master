59b7b4c269d73b34fea59e5fc46ad28fdc79fd23
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-18 15:46:40 +0800

    added search form with constructor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,12 +24,18 @@ class App extends Component {
             <FormControl
             type="text"
             placeholder="Search an artist..."
+            value={this.state.query}
+            onChange={ event => {this.setState({query: event.target.value})}}
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                this.search()
+              }
+            }}
             />
-            <Button variant="outline-secondary">
+            <Button variant="outline-secondary" onClick={() => this.search()}>
               <BsSearch />
             </Button>
           </InputGroup>
-
         </FormGroup>
         <div className="profile">
           <div>Artist picture</div>
