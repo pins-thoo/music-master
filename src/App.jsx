50f19988f080cb5f764b6d650b3e68c83455be29
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-18 16:17:52 +0800

    added spotify api

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,18 @@ class App extends Component {
 
   search() {
     console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log('FETCH_URL', FETCH_URL);
+    fetch(FETCH_URL, {
+    method: 'GET',
+    headers: new Headers({
+    'Authorization': 'Bearer BQCw22HzJlIaG_xdSKOZFRVTj-kYKqGwE_wOFvE1kyYcLPvRI50JRiwX_gS-ps156RJ4VP_1fQmoi65Ot90y7Rbwr6HV1Uo61_2a_EJgh5S6O4GQRZUGxXdI-JAwkDStNiHjIyMa9Gm_-TA'
+    }),
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json))
+    .catch(error => console.log('error', error))
   }
 
   render() {
@@ -32,9 +44,11 @@ class App extends Component {
               }
             }}
             />
-            <Button variant="outline-secondary" onClick={() => this.search()}>
-              <BsSearch />
-            </Button>
+            <InputGroup.Append>
+              <Button variant="outline-secondary" onClick={() => this.search()}>
+                <BsSearch />
+              </Button>
+            </InputGroup.Append>
           </InputGroup>
         </FormGroup>
         <div className="profile">
